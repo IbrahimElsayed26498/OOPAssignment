407b59870894980dcc275b7f8f1ccf742319a013
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2021-05-01 20:59:26 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         DeckOfCards cards = new DeckOfCards();
         cards.fillDeckOfCards();
-        for (int i=0; i<52; i++)
+        for (int i=0; i<10; i++)
             cards.displayCard(i);
-        //cards.displayDeckOfCards();
     }
 }
